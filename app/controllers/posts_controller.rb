4fe584faec6a22056f57d7fdb56de74d6db3ce68
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-27 00:01:35 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,6 @@ class PostsController < ApplicationController
   def show
   end
 
-  def body
-    post = Post.find(params[:id])
-    render plain: post.description
-  end
 
   def new
     @post = Post.new
@@ -29,6 +25,11 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
+  end
+
+  def body
+    post = Post.find(params[:id])
+    render plain: post.description
   end
 
 private
